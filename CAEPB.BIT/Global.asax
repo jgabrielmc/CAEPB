171f63cf13015328c8c937b59f94124f5efdfc6f
Jose Medina <Jose Medina@BP-PC-08>
2017-08-04 17:10:16 -0500

    Add project files.

---
+++ CAEPB.BIT/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CAEPB.BIT.MvcApplication" Language="C#" %>
---
+++ CAEPB.BIT/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CAEPB.BIT.Startup))]
+namespace CAEPB.BIT
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
